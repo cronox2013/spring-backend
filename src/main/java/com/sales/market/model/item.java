f87e91584d47a81a8ee3c63f46ab940d4ce0ffad
cronox2013 <andypandaag@gmail.com>
2022-04-14 17:06:27 -0400

    modelo del negocio y primera entidad

---
+++ src/main/java/com/sales/market/model/item.java
@@ -0,0 +1,41 @@
+package com.sales.market.model;
+
+public class item {
+    private String name;
+    private String code;
+    private Byte[] image;
+    private Subcategory subcategory;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Subcategory getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(Subcategory subcategory) {
+        this.subcategory = subcategory;
+    }
+}
+
